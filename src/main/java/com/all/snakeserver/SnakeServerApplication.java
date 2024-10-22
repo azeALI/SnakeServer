f42efdeaadb16dac9d190586de12ece1e49be3e1
azeALI <alihesenzade5000@gmail.com>
2024-10-22 12:27:29 +0400

    first commit

---
+++ src/main/java/com/all/snakeserver/SnakeServerApplication.java
@@ -0,0 +1,13 @@
+package com.all.snakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/all/snakeserver/SnakeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all.snakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
